5b42a7166c0ad4fba4c3cc34d5dd285c2a53484d
Jeansonne <jeanso_a1@red5.cs.denison.edu>
2024-03-27 14:11:46 -0400

    Added frequency count file.

--- countFrequency.py
+++ countFrequency.py
@@ -2,3 +2,22 @@
 # reads the WarPeace.txt file (via stdin redirection) and 
 # counts instances of letters (converted to lower case). 
 # outputs a frequency table stores in a textfile. 
+
+
+freq = dict()
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for char in alphabet:
+    freq[char] = 0
+with open("WarPeace.txt") as file:
+    for line in file:
+        for char in line:
+            char = char.lower()
+            if char in alphabet:
+                freq[char] = freq[char] + 1
+
+freqText = ''
+for char in alphabet:
+    freqText += f'{freq[char]}\n'
+freqText = freqText[:-1]
+with open('frequency.txt', 'w') as file:
+    file.write(freqText)
