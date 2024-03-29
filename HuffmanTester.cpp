9ec8950c8c08b3d7fe1e849d42cbcae4420233ab
Wisnefski <wisnef_m1@red6.cs.denison.edu>
2024-03-28 21:03:43 -0400

    now takes .txt file as command line input

--- HuffmanTester.cpp
+++ HuffmanTester.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include "HuffmanCode.h"
+#include "HuffmanCode.cpp"
 #include "BinaryTree.h"
+#include <fstream>
 
 using namespace std;
 
-int main() {
-    // Example string of frequencies
-    string counts = "204420\n34372\n60660\n117752\n313026\n54507\n50909\n166520\n172641\n2485\n20282\n96036\n61283\n183130\n191492\n44717\n2319\n146890\n162130\n224510\n64918\n26788\n58930\n4032\n45937\n2385";
+int main(int argc, char *argv[]) {
+
+    if (argc < 1){
+        cout << "Error: no file entered in command line" << endl;
+        return -1;
+    }
+    
+    string filename = argv[1]; 
+    //cout << filename; 
+    fstream f;
+    f.open(filename);
+    //read the 
+    string counts = "";
+    string tp;
+    while(getline(f, tp)){ //reading each line of data
+         //cout << tp << "\n"; 
+         //adding each line of data to out counts string
+         counts = counts + tp + "\n";
+    }
+    cout << counts << endl;
+
+    f.close();
+
+    
+
+    // Example string of frequencies (used before we used command line input)
+    //string counts = "204420\n34372\n60660\n117752\n313026\n54507\n50909\n166520\n172641\n2485\n20282\n96036\n61283\n183130\n191492\n44717\n2319\n146890\n162130\n224510\n64918\n26788\n58930\n4032\n45937\n2385";
 
     // Create HuffmanCode object
     HuffmanCode huffman;
@@ -25,7 +51,7 @@ int main() {
     // cout << encodings.at('a');
 
     string s = huffman.encodingsToString(encodings);
-    cout << s;
+    cout << s << endl;
 
 
 
