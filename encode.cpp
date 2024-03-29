9249670a7aef615cab41115617f36f22c4c31861
Wisnefski <wisnef_m1@red6.cs.denison.edu>
2024-03-28 21:55:23 -0400

    encoding string of text into binary code

--- encode.cpp
+++
@@ -1,6 +0,0 @@
-//============================================================================
-// Hieu Tran, Max Wisnefski, Aidan Jeansonne
-// March 2024
-// encode.cpp
-// This application encodes a .txt file and prints the binary string
-//============================================================================
---
+++ encode.py
@@ -0,0 +1,31 @@
+# Hieu Tran, Max Wisnefski, Aidan Jeansonne
+# March 2024
+# encode.py
+# this program reads the code vectors from the text file.  
+# It then reads some block of text and translates the block of text into a code.
+
+text = "this text is just to test the encoding\nabcdefghijklmnopqrstuvwxyz"
+
+codes_dict = {}
+
+#creating a dictionary where the keys are letters,
+#and the values are the codes for the letters
+with open("encodings.txt") as file:
+    for line in file:
+        #print(line[2:-1])
+        let = line[0]
+        code = line[2:-1]
+        codes_dict[let] = code
+
+#print(codes_dict)
+keys = codes_dict.keys()
+
+encoded_txt = ""
+for char in text:
+    if char == "\n":
+        encoded_txt = encoded_txt + "\n"
+    for key in keys:
+        if char == key:
+            encoded_txt = encoded_txt + codes_dict[key] + " "
+
+print(encoded_txt)
